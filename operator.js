58a96440bda632daf506fa88d5619b693e0b1e3d
mansisni <mansisoni1407@gmail.com>
2022-07-25 17:46:34 +0530

    adding final code

---
+++ operator.js
@@ -0,0 +1,30 @@
+function nirali(){
+    var a = parseInt(document.forms["M"]["c1"].value)
+    var b = parseInt(document.forms["M"]["c2"].value)
+    var add = a + b
+    alert(add)
+}
+function mansi(){
+    var c = document.forms["M"]["c1"].value
+    var d = document.forms["M"]["c2"].value
+    var sub = c - d
+    alert(sub)
+}
+function princi(){
+    var e = document.forms["M"]["c1"].value
+    var f = document.forms["M"]["c2"].value
+    var mul = e * f
+    alert(mul)
+}
+function pappa(){
+    var g = document.forms["M"]["c1"].value
+    var h = document.forms["M"]["c2"].value
+    var div = g / h
+    alert(div) 
+}
+function mummy(){
+    var i = document.forms["M"]["c1"].value
+    var j = document.forms['M']["c2"].value
+    var circ = Math.pow(i,j)
+    alert(circ)
+}
